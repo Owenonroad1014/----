8977bcac2a9b1557f44dd80a44d2b5545fa7e5c0
Owen <dtes95105@gmail.com>
2025-01-10 16:28:49 +0800

    second-commit

--- pages/includes/head.php
+++ pages/includes/head.php
@@ -45,7 +45,9 @@ require_once 'includes/session.php';
 <body>
     <nav class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme" id="layout-navbar">
         <div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0">
-           
+        <a class="nav-item nav-link px-0 me-xl-6" href="javascript:void(0)">
+          <i class="bx bx-menu bx-md"></i>
+        </a>
         </div>
 
         <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
